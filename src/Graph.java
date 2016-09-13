1d884acecc2d3fbca1f0e01c63afae8b24843b5a
sorvachev <ssorvachev@gmail.com>
2016-09-13 12:53:08 +0300

    try build graph

--- src/Graph.java
+++ src/Graph.java
@@ -7,7 +7,7 @@ import java.util.*;
 /**
  * Created by sakic on 9/13/16.
  */
-public class Graph   {
+public class Graph {
     private static final Color[] FUNCTION_COLORS = {Color.RED, new Color(0, 127, 0), Color.BLUE};
     private static final int SCALE_INTERVAL = 100;
     private static final int CACHE_SIZE = 100;
@@ -50,5 +50,83 @@ public class Graph   {
     public Graph(Function... functions) {
         f = Arrays.copyOf(functions, functions.length);
         fCache = new double[functions.length][CACHE_SIZE];
+    }
+
+    private BufferedImage getImageAt(int x, int y) {
+        CPoint p = new CPoint(x, y);
+
+        recentPoints.remove(p);
+        recentPoints.add(p);
+        while (recentPoints.size() > Q_SIZE)
+            recentPoints.remove();
+
+        BufferedImage img;
+
+        img = cache.get(p);
+        if (img != null)
+            return img;
+        else {
+            double a = toGraphScaleY(y * CACHE_SIZE);
+            double b = toGraphScaleY((y + 1) * CACHE_SIZE);
+            double yMin = Math.min(a, b);
+            double yMax = Math.max(a, b) + 1;
+            boolean canUseEmptyImage = true;
+
+            if ((x >= -1 && x <= 0) || (y >= -1 && y <= 0))
+                canUseEmptyImage = false;
+            else {
+                fillCache(x);
+                test_loop:
+                for (int fi = 0; fi < f.length; fi++) {
+                    Function func = f[fi];
+                    double prevVal = func.getApproximation(toGraphScaleX(x * CACHE_SIZE - 1));
+                    int xMin = x * CACHE_SIZE;
+                    int xMax = (x + 1) * CACHE_SIZE;
+                    for (int i = xMin - 1; i <= xMax + 1; i++) {
+                        double val;
+                        if (i >= xMin && i < xMax)
+                            val = fCache[fi][i - xMin];
+                        else
+                            val = func.getApproximation(toGraphScaleX(i));
+                        if ((val >= yMin && val <= yMax) || (val < yMin) != (prevVal < yMin)) {
+                            canUseEmptyImage = false;
+                            break test_loop;
+                        }
+                    }
+                }
+            }
+
+        }
+return new BufferedImage();
+    }
+    private static boolean isValidNumber(double x)
+    {
+        return !(Double.isNaN(x) || Double.isInfinite(x));
+    }
+
+    public int toPixelsX(double x)
+    {
+        return (int) (x * SCALE_INTERVAL / xScale);
+    }
+
+    public int toPixelsY(double x)
+    {
+        return -(int) (x * SCALE_INTERVAL / yScale);
+    }
+
+    public double toGraphScaleX(int x)
+    {
+        double s = x;
+        s /= SCALE_INTERVAL;
+        s *= xScale;
+        return s;
+    }
+
+    public double toGraphScaleY(int x)
+    {
+        double s = x;
+        s /= SCALE_INTERVAL;
+        s *= yScale;
+        return -s;
     }
 }
